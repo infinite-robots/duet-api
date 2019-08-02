87425c4558955484afcba29c6fd939fd6f34061c
nicholashopper <nicholashopper@iheartmedia.com>
2019-08-01 22:09:30 -0500

    Fix unseenBands returning interests instead of bands

--- service/BandService.js
+++ service/BandService.js
@@ -20,6 +20,12 @@ class BandService {
       }
     });
   }
+
+  async getUnseenBands(id) {
+    const interests = (await MusicInterest.findAll({where: {user_id: id}})).map(band => band.band_id);
+    const unseen = await Band.findAll().filter(band => !interests.includes(band.id));
+    return unseen;
+  }
 }
 
 module.exports = { BandService };
